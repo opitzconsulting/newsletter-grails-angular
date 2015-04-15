9159838d2d94f7dcc0d926f5ab8cde21938016cc
Christian Olsson <colsson1990@gmail.com>
2015-04-15 09:45:04 +0200

    Added Markdown-domain

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,11 +1,13 @@
 import newsletter.grails.Customer
+import newsletter.grails.Markdown
 
 class BootStrap {
 
     def init = { servletContext ->
-    Customer.findOrSaveWhere(firstname: 'John', lastname: 'Doe')
-    Customer.findOrSaveWhere(firstname: 'Oli', lastname: 'Wagner')
-    Customer.findOrSaveWhere(firstname: 'Sarah', lastname: 'Schmitz')
+        Customer.findOrSaveWhere(firstname: 'John', lastname: 'Doe')
+        Customer.findOrSaveWhere(firstname: 'Oli', lastname: 'Wagner')
+        Customer.findOrSaveWhere(firstname: 'Sarah', lastname: 'Schmitz')
+        Markdown.findOrSaveWhere(text: '#Newsletter   * list element 1   * list element 2  ``This is a code block.`` > A blockquote   **This is bold**')
     }
     def destroy = {
     }
